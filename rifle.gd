9a6a7968285aeab0d1bce54d64721bccbe7ec5e8
reign <reign@Big-OOO.local>
2026-06-16 13:35:24 -0500

    create rifle hitscan weapon object

---
+++ rifle.gd
@@ -0,0 +1,25 @@
+extends NetworkWeaponHitscan3D
+class_name Rifle
+
+@export var fire_action: GUIDEAction
+
+@export var player: Player
+@onready var player_peer_id: int = player.name.to_int()
+
+func _ready() -> void:
+	fire_action.just_triggered.connect(fire)
+
+func _can_peer_use(peer_id: int) -> bool:
+	return peer_id == player_peer_id
+
+func _can_fire() -> bool:
+	return true
+
+func _on_fire():
+	if multiplayer.get_unique_id() == player_peer_id:
+		print("bang")
+
+func _on_hit(result: Dictionary) -> void:
+	if result["collider"] is Player:
+		if multiplayer.get_unique_id() == player_peer_id:
+			print("hit")
