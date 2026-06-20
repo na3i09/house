88a618d3fc9f3f139986e48ba5e41eb990e08ed9
reign <reign@Big-OOO.local>
2026-06-20 11:19:29 -0500

    add enum seletor for beam scan mode

--- tracker.gd
+++ tracker.gd
@@ -1,24 +1,42 @@
 extends Node3D
 class_name Tracker
 
+enum ScanMode {
+	SWEEP,
+	INSTANT,
+	CONTINUOUS,
+}
+
 @export var player: Player
 
 @export var action: GUIDEAction
 
 @export_group("Settings")
-@export var continuous_scan: bool = false
 @export var enable_scan: bool = true
 @export var enable_ping: bool = false
+@export_enum("Sweep","Instant","Continuous") var scan_mode: int
 
 @onready var ping_quad: MeshInstance3D = $PingQuad
 @onready var scanner: Scanner = $Scanner
 
 func _ready() -> void:
 	set_multiplayer_authority(player.name.to_int())
-	if continuous_scan:
-		action.triggered.connect(_fire_beam)
-	else:
-		action.just_triggered.connect(_fire_tracker)
+	_set_beam_mode(scan_mode)
+
+func _set_beam_mode(_scan_mode: ScanMode) -> void:
+	if action.triggered.is_connected(_fire_beam):
+		action.triggered.disconnect(_fire_beam)
+	if action.just_triggered.is_connected(_fire_tracker):
+		action.just_triggered.disconnect(_fire_tracker)
+	match _scan_mode:
+		ScanMode.SWEEP:
+			action.just_triggered.connect(_fire_tracker)
+			scanner.instant = false
+		ScanMode.INSTANT:
+			action.just_triggered.connect(_fire_tracker)
+			scanner.instant = true
+		ScanMode.CONTINUOUS:
+			action.triggered.connect(_fire_beam)
 
 func _fire_tracker() -> void:
 	if is_multiplayer_authority():
