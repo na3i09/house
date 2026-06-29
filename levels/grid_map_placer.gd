36553772790badea5fd0a6b98456e094a38f0096
reign <reign@Big-OOO.local>
2026-06-29 13:46:21 -0500

    add support for placing scenes in specific grid map locations

--- levels/grid_map_placer.gd
+++ levels/grid_map_placer.gd
@@ -4,6 +4,8 @@ class_name GridMapPlacer
 @export var place_dict: Dictionary[int,PackedScene]
 @export var verticle_offset: float = 0.0
 
+@export var location_place_dict: Dictionary[Vector3i,PackedScene]
+
 func _ready() -> void:
 	for index in place_dict:
 		var instance_array: Array[Vector3i] = get_used_cells_by_item(index)
@@ -14,3 +16,11 @@ func _ready() -> void:
 			inst_location.y += verticle_offset
 			inst_scene.global_position = inst_location
 			print(inst_scene.global_position)
+	
+	for location in location_place_dict:
+		var inst_scene = location_place_dict[location].instantiate() as Node3D
+		add_child(inst_scene)
+		var inst_location: Vector3 = to_global(map_to_local(location))
+		inst_location.y += verticle_offset
+		inst_scene.global_position = inst_location
+		print(inst_scene.global_position)
