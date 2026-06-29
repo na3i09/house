ab8a53b2873cc2bac67ae799d0d6e9a91e9175fe
reign <reign@Big-OOO.local>
2026-06-29 13:37:54 -0500

    add script for grid map with support for placing scenes at specified grid map elements

---
+++ levels/grid_map_placer.gd
@@ -0,0 +1,16 @@
+extends GridMap
+class_name GridMapPlacer
+
+@export var place_dict: Dictionary[int,PackedScene]
+@export var verticle_offset: float = 0.0
+
+func _ready() -> void:
+	for index in place_dict:
+		var instance_array: Array[Vector3i] = get_used_cells_by_item(index)
+		for inst in instance_array:
+			var inst_scene = place_dict[index].instantiate() as Node3D
+			add_child(inst_scene)
+			var inst_location: Vector3 = to_global(map_to_local(inst))
+			inst_location.y += verticle_offset
+			inst_scene.global_position = inst_location
+			print(inst_scene.global_position)
