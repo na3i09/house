3d62db1e5bb8f9f1aa6564a6afa51610168b6ff0
reign <reign@Big-OOO.local>
2026-07-07 20:30:23 -0500

    add members to store map dimensions

--- components/grid_map_configuration.gd
+++ components/grid_map_configuration.gd
@@ -5,6 +5,15 @@ class_name GridMapConfiguration
 
 @export_storage var edge_locations: Dictionary[Vector3i,int]
 
+@export_storage var map_minimum: Vector3i = Vector3i.ZERO
+
+@export_storage var map_maximum: Vector3i = Vector3i.ZERO
+
+var map_size: Vector3i:
+	get:
+		return map_maximum - map_minimum
+	set(value):
+		return
 
 static func generate_configuration_resource(configuration: Dictionary[Vector3i,Array], edge_id: int) -> GridMapConfiguration:
 	var config_resource: GridMapConfiguration = GridMapConfiguration.new()
@@ -16,5 +25,21 @@ static func generate_configuration_resource(configuration: Dictionary[Vector3i,A
 			if configuration[location][0] == edge_id:
 				config_resource.edge_locations[location] = configuration[location][1]
 				config_resource.configuration_dict.erase(location)
+	
+	#TODO: replace this with something less dumb
+	for location in config_resource.configuration_dict:
+		if location.x > config_resource.map_maximum.x:
+			config_resource.map_maximum.x = location.x
+		if location.y > config_resource.map_maximum.y:
+			config_resource.map_maximum.y = location.y
+		if location.z > config_resource.map_maximum.z:
+			config_resource.map_maximum.z = location.z
 		
+		if location.x < config_resource.map_minimum.x:
+			config_resource.map_minimum.x = location.x
+		if location.y < config_resource.map_minimum.y:
+			config_resource.map_minimum.y = location.y
+		if location.z < config_resource.map_minimum.z:
+			config_resource.map_minimum.z = location.z
+	
 	return config_resource
