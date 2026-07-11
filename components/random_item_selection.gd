7381d29e64b8d326e9a8d97089faa9293968f93d
reign <reign@Big-OOO.local>
2026-07-11 15:13:08 -0500

    add documentation comments for random item selection

--- components/random_item_selection.gd
+++ components/random_item_selection.gd
@@ -4,17 +4,26 @@ class_name RandomItemSelection
 ## defining what tile the item can spawn on, its chance to spawn, and its range of offset transform.
 
 
+## Name of the spawned item
 @export var spawned_item: StringName
+## Name of the tile type to spawn item on
 @export var spawned_tile: StringName
+## Chance for item to spawn
 @export var spawn_chance: float = 0.2
 
+## Minimum rotational offset for spawned item
 @export_range(-180,0,0.1,"radians_as_degrees") var min_rotation: float = deg_to_rad(0.0)
+## Maximum rotational offset for spawned item
 @export_range(0,180,0.1,"radians_as_degrees") var max_rotation: float = deg_to_rad(0.0)
+## Axis of rotation for rotational offset
 @export var rotation_axis: Vector3 = Vector3.UP
 
+## Minimum location offset for spawned item
 @export var min_location_offset: Vector3 = Vector3.ZERO
+## Maximum location offset for spawned item
 @export var max_location_offset: Vector3 = Vector3.ZERO
 
+## Create an instance of the random item returning an item transform pair
 func instance_item() -> Array:
 	if randf() > spawn_chance:
 		return []
