681034433518f730e3a70679f2408077b951afbd
reign <reign@Big-OOO.local>
2026-07-14 13:47:51 -0500

    add spinbox to set number of segments to generate

--- addons/minos_mapper/minos_bottom_panel.gd
+++ addons/minos_mapper/minos_bottom_panel.gd
@@ -1,8 +1,9 @@
 @tool
 extends EditorDock
 
+@export var generation_segments: SpinBox
 
 func _on_generate_button_pressed() -> void:
 	var map_placer: GridMapPlacer = EditorInterface.get_inspector().get_edited_object() as GridMapPlacer
 	if map_placer:
-		map_placer._generate()
+		map_placer._generate(int(generation_segments.value))
