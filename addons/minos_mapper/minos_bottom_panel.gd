cb14e493b7c345374d93b153b98f7716a6faf138
reign <reign@Big-OOO.local>
2026-07-14 13:39:07 -0500

    add functionality to bottom panel item to generate map

--- addons/minos_mapper/minos_bottom_panel.gd
+++ addons/minos_mapper/minos_bottom_panel.gd
@@ -2,11 +2,7 @@
 extends EditorDock
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_generate_button_pressed() -> void:
+	var map_placer: GridMapPlacer = EditorInterface.get_inspector().get_edited_object() as GridMapPlacer
+	if map_placer:
+		map_placer._generate()
