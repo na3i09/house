0bc24cfc3060610f62def68ca1863f2ac66a178f
reign <reign@Big-OOO.local>
2026-07-15 10:13:45 -0500

    make plugin coppies of grid map placer classes

---
+++ addons/minos_mapper/minos_map_configuration.gd
@@ -0,0 +1,43 @@
+extends Resource
+class_name MinosMapConfiguration
+
+const EDGE_NAME: StringName = "Edge"
+
+@export_storage var configuration_dict: Dictionary[Vector3i,Array]
+
+@export_storage var edge_locations: Dictionary[Vector3i,int]
+
+@export_storage var map_minimum: Vector3i = Vector3i.ZERO
+
+@export_storage var map_maximum: Vector3i = Vector3i.ZERO
+
+var map_size: Vector3i:
+	get:
+		return map_maximum - map_minimum
+	set(value):
+		return
+
+## Create a [MinosMapConfiguration] based on the given [param configuration] with the [param mesh_library] argument
+## to infer the tile id of edges of the map segment to be used in map generation
+static func generate_configuration_resource(configuration: Dictionary[Vector3i,Array], mesh_library: MeshLibrary) -> MinosMapConfiguration:
+	var config_resource: MinosMapConfiguration = MinosMapConfiguration.new()
+	var edge_id: int = mesh_library.find_item_by_name(EDGE_NAME)
+	
+	config_resource.configuration_dict = configuration.duplicate()
+	
+	if edge_id != -1:
+		for location in configuration.keys():
+			if configuration[location][0] == edge_id:
+				config_resource.edge_locations[location] = configuration[location][1]
+				config_resource.configuration_dict.erase(location)
+	
+	config_resource.map_maximum = config_resource.configuration_dict.keys().reduce(_max_vector)
+	config_resource.map_minimum = config_resource.configuration_dict.keys().reduce(_min_vector)
+	
+	return config_resource
+
+static func _max_vector(accum: Vector3i, element: Vector3i) -> Vector3i:
+	return accum.max(element)
+
+static func _min_vector(accum: Vector3i, element: Vector3i) -> Vector3i:
+	return accum.min(element)
