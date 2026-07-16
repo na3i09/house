7b97f900d4bf2a7215e64b0169a0d747f1fa354f
reign <reign@Big-OOO.local>
2026-07-16 13:23:44 -0500

    remove unused bake meshes functionality

--- addons/minos_mapper/minos_mapper.gd
+++ addons/minos_mapper/minos_mapper.gd
@@ -172,32 +172,6 @@ func _remove_export_as_entry(menu: PopupMenu) -> void:
 	menu.remove_item(menu_item_index)
 
 
-func _run_bake_meshes(...args) -> void:
-	_bake_meshes()
-
-
-func _bake_meshes() -> void:
-	var scene_root: Node = EditorInterface.get_edited_scene_root()
-	
-	var csgs: Array = scene_root.find_children("*","CSGShape3D",false)
-	
-	for csg: CSGShape3D in csgs:
-		var baked_mesh: ArrayMesh = csg.bake_static_mesh()
-		
-		var mesh_inst := MeshInstance3D.new()
-		mesh_inst.mesh = baked_mesh
-		mesh_inst.name = csg.name.replace("CSG","")
-		if scene_root.find_child(mesh_inst.name):
-			var old_inst := scene_root.find_child(mesh_inst.name)
-			old_inst.name = old_inst.name + "_old"
-			old_inst.queue_free()
-		scene_root.add_child(mesh_inst)
-		mesh_inst.global_position = csg.global_position
-		mesh_inst.owner = scene_root
-		
-		mesh_inst.create_trimesh_collision()
-
-
 func _show_minos_mesh_save_dialog() -> void:
 	minos_mesh_save_dialog.popup_centered_clamped(Vector2i(700,500))
 
